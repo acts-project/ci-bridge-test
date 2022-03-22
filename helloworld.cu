3bc2da5f7400cc4be83d98eccdf1fe27058874bc
Benjamin Huth <benjamin.huth@physik.uni-regensburg.de>
2022-03-22 10:02:04 +0100

    update

--- helloworld.cu
+++ helloworld.cu
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <iostream>
 
 #define N 10000000
 #define MAX_ERR 1e-6
@@ -14,7 +15,40 @@ __global__ void vector_add(float *out, float *a, float *b, int n) {
     }
 }
 
+void display_infos()
+{
+    const int kb = 1024;
+    const int mb = kb * kb;
+    std::cout << "NBody.GPU" << std::endl << "=========" << std::endl << std::endl;
+
+    std::cout << "CUDA version:   v" << CUDART_VERSION << std::endl;
+    std::cout << "Thrust version: v" << THRUST_MAJOR_VERSION << "." << THRUST_MINOR_VERSION << std::endl << std::endl;
+
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+    std::cout << "CUDA Devices: " << std::endl << std::endl;
+
+    for(int i = 0; i < devCount; ++i)
+    {
+        cudaDeviceProp props;
+        cudaGetDeviceProperties(&props, i);
+        std::cout << i << ": " << props.name << ": " << props.major << "." << props.minor << std::endl;
+        std::cout << "  Global memory:   " << props.totalGlobalMem / mb << "mb" << std::endl;
+        std::cout << "  Shared memory:   " << props.sharedMemPerBlock / kb << "kb" << std::endl;
+        std::cout << "  Constant memory: " << props.totalConstMem / kb << "kb" << std::endl;
+        std::cout << "  Block registers: " << props.regsPerBlock << std::endl << std::endl;
+
+        std::cout << "  Warp size:         " << props.warpSize << std::endl;
+        std::cout << "  Threads per block: " << props.maxThreadsPerBlock << std::endl;
+        std::cout << "  Max block dimensions: [ " << props.maxThreadsDim[0] << ", " << props.maxThreadsDim[1]  << ", " << props.maxThreadsDim[2] << " ]" << std::endl;
+        std::cout << "  Max grid dimensions:  [ " << props.maxGridSize[0] << ", " << props.maxGridSize[1]  << ", " << props.maxGridSize[2] << " ]" << std::endl;
+        std::cout << std::endl;
+    }
+}
+
 int main(){
+    display_infos();
+
     float *a, *b, *out;
     float *d_a, *d_b, *d_out; 
 
@@ -46,7 +80,10 @@ int main(){
 
     // Verification
     for(int i = 0; i < N; i++){
-        assert(fabs(out[i] - a[i] - b[i]) < MAX_ERR);
+        if(fabs(out[i] - a[i] - b[i]) < MAX_ERR) {
+            printf("ERROR in verification");
+            return 0;
+        }
     }
     printf("out[0] = %f\n", out[0]);
     printf("PASSED\n");
